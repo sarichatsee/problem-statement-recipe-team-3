a5e3043f8daf05f8630bc964b2eb498418a5fbac
ymyeemon <23008873@myrp.edu.sg>
2025-02-04 11:01:56 +0800

    commit

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,1 +1,20 @@
+import { useAuthContext } from "./useAuthContext";
+import { useRecipeContext } from "./useRecipeContext";
 
+export const useLogout = () => {
+
+  const { dispatch} = useAuthContext();
+  const { dispatch: recipeDispatch } = useRecipeContext();
+
+  const logout = () => {
+    localStorage.removeItem('user');
+
+    dispatch({
+      type: 'LOGOUT'
+    });
+
+    recipeDispatch({ type: 'SET_RECIPES', payload: null });
+  }
+
+  return { logout };
+}
